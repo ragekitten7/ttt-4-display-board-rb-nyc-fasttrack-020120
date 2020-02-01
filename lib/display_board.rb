319ce817eb755b31c8cfa4c58c2e9afaf0d3b633
Sam Bryant <bryantsj@gmail.com>
2020-02-01 18:22:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,15 +4,12 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 
 
 def display_board(board)
-
-  puts " #{board[0]} ","|"," #{board[1]} ","|"," #{board[2]} "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{board[3]} ","|"," #{board[4]} ","|"," #{board[5]} "
+  puts "   |   |   "
   puts "-----------"
-  puts " #{board[6]} ","|"," #{board[7]} ","|"," #{board[8]} "
-end
-
-
+  puts "   |   |   "
+end 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " ",]
 board = ["O", " ", " ", " ", " ", " ", " ", " ", " ",]
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " ",]
